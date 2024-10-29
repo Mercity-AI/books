fa092f7f4bba5119d76fb5fe2a099147795a4530
Sirri69 <pranav2278@gmail.com>
2024-10-29 17:38:45 +0530

    Updated the base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "http://books.mercity.ai/books/",
+    baseUrl: "books.mercity.ai/books/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
